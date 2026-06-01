913dacb2b7cad9e7cd89b46095e92f94333be712
Apurv Musandi <apurvmusandi@gmail.com>
2026-06-01 22:24:32 +0530

    Initial Commit

---
+++ Test.java
@@ -0,0 +1,8 @@
+public class Test {
+    public static void main(String[] args) {
+        //System.out.println("Hello, BKL!");
+        //System.out.println("Hey,Loies!");
+        System.out.println("Meg: Destroy Me!");
+        System.out.println("Chris: ok..........");
+    }
+}
